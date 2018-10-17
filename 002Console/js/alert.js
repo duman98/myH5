31d223ca7ef4b23cbceb4d149525d57056243e9f
dumen <dumenpan@google.com>
2018-10-17 14:38:23 +0800

    modify

---
+++ 002Console/js/alert.js
@@ -0,0 +1,15 @@
+/*这个中二的网页是oeasy写的....,在webstorm中Ctrl+/可以快速注释*/
+function tcLu(){
+    alert("你猜一会儿会发生什么");
+    alert("再等等");
+    alert("也许真有奇迹");
+}
+alert("你怎么打开了这个网页")
+tcLu();
+alert("你相信奇迹吗")
+alert("让我来向你提个问题")
+tcLu();
+alert("果然没有奇迹");
+alert("我们就这样放弃吗？");
+tcLu();
+alert("这是最后一句了，88");
